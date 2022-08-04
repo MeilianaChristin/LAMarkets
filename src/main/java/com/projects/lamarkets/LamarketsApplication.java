686c36c587302a7a5896d9f080be13d48417b65c
christin meiliana <ext.enigma6@cashlez.com>
2022-08-04 15:13:01 +0700

    first commit

---
+++ src/main/java/com/projects/lamarkets/LamarketsApplication.java
@@ -0,0 +1,13 @@
+package com.projects.lamarkets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamarketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LamarketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/lamarkets/LamarketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.lamarkets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamarketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
